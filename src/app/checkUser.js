dfb45120c8037905ce4da0c3cc3d8df47c7b9524
Dardo Altobelli <altobellidardo@gmail.com>
2024-05-14 22:45:16 -0300

    add new pages

--- src/app/checkUser.js
+++ src/app/checkUser.js
@@ -5,6 +5,9 @@ export const checkUser = (user) => {
   if (user) {
     elementsNoSession.forEach(element => element.classList.add('d-none'))
     elementsSession.forEach(element => element.classList.remove('d-none'))
+
+    const output = document.getElementById('output')
+    output.getElementsByTagName('p')[0].textContent += user.email
   } else {
     elementsNoSession.forEach(element => element.classList.remove('d-none'))
     elementsSession.forEach(element => element.classList.add('d-none'))
